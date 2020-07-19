9fda56a256b41ab79f4ef518a631608cb9e3acfb
reactionaryzebra <reactionaryzebra@gmail.com>
2020-07-19 11:08:10 -0700

    set up erb file instead of static html, and pass a name param from the hello_world controller to the view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   def hello_world
-    render 'application/hello_world'
+    name = params['name']
+    render 'application/hello_world', locals: { name: name }
   end
 end
