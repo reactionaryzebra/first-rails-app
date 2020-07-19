87a61ac3df0cab31f49f01d3caca5555b3a7c811
reactionaryzebra <reactionaryzebra@gmail.com>
2020-07-19 11:39:28 -0700

    setup show route and controller for single post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,14 @@ class ApplicationController < ActionController::Base
     posts = connection.execute("SELECT * FROM posts")
     render 'application/list_posts', locals: { posts: posts }
   end
+  def show_post
+    post_id = params['id']
+
+    connection = SQLite3::Database.new 'db/development.sqlite3'
+    connection.results_as_hash = true
+
+    post = connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", post_id).first
+
+    render plain: post['title']
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/hello_world' => 'application#hello_world'
   get '/hello/:name' => 'application#hello_world'
+  get '/posts/:id' => 'application#show_post'
   get '/posts' => 'application#list_posts'
 end
