ca73ca665e7d6369b784652382fe8babc0dbda0e
reactionaryzebra <reactionaryzebra@gmail.com>
2020-07-19 11:48:43 -0700

    extract db connection logic into separate private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,22 @@ class ApplicationController < ActionController::Base
     render 'application/hello_world', locals: { name: name }
   end
   def list_posts
-    connection = SQLite3::Database.new 'db/development.sqlite3'
-    connection.results_as_hash = true
-
     posts = connection.execute("SELECT * FROM posts")
     render 'application/list_posts', locals: { posts: posts }
   end
   def show_post
     post_id = params['id']
 
-    connection = SQLite3::Database.new 'db/development.sqlite3'
-    connection.results_as_hash = true
-
     post = connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", post_id).first
 
     render 'application/show_post', locals: { post: post }
+  end
+
+  private
+
+  def connection
+    db_connection = SQLite3::Database.new 'db/development.sqlite3'
+    db_connection.results_as_hash = true
+    db_connection
   end
 end
