90480d54bdca0190b69a9ae67aeab813456aa00f
reactionaryzebra <reactionaryzebra@gmail.com>
2020-07-20 08:51:58 -0700

    add connection method to BlogPost model to maintain connection to the db

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -21,4 +21,10 @@ class BlogPost
       author,
       Date.current.to_s
   end
+
+  def connection
+    db_connection = SQLite3::Database.new 'db/development.sqlite3'
+    db_connection.results_as_hash = true
+    db_connection
+  end
 end
